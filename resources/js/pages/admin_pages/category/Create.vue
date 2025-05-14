45aa2bfc1d8675132aa686fbeb0268d46851f1cf
Sahin Islam <sahinislam101@gmail.com>
2025-05-14 09:20:37 +0600

    brad

--- resources/js/pages/admin_pages/category/Create.vue
+++ resources/js/pages/admin_pages/category/Create.vue
@@ -74,7 +74,7 @@ const breadcrumbs: BreadcrumbItem[] = [
           </button>
 
           <Link
-            href="/sliders"
+            href="/categories"
             class="inline-flex items-center px-4 py-2 bg-gray-200 text-gray-700 hover:bg-gray-300 rounded-md transition"
           >
             Back
